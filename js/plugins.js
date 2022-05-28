1e62093965f932a22c08c2a38cc439b685710c93
Mohamed Ashraf <botoashraf123@gmail.com>
2022-05-28 15:38:36 +0200

    update the old repo

--- js/plugins.js
+++ js/plugins.js
@@ -1,5 +1,82 @@
-$(document).ready(function() {
+$(document).ready(function () {
   "use strict";
+
+  $(".owl-carousel").owlCarousel({
+    //Responsive
+    loop: false,
+    margin: 10,
+    nav: false,
+    dots: true,  
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      300: {
+        items: 2,
+        stagePadding: 1,
+      },
+      480: {
+        items: 2,
+        stagePadding: 18,
+      },
+      600: {
+        items: 2,
+        stagePadding: 18,
+      },
+      1000: {
+        items: 5,
+        stagePadding: 0,
+        margin: 18,
+        navText: [
+          "<i class='fa fa-chevron-left'></i>",
+          "<i class='fa fa-chevron-right'></i>",
+        ],
+        nav: true,
+        mouseDrag: true,
+        touchDrage: true,
+      },
+    },
+  });
+
+  $(".owl-lg-4").owlCarousel({
+    //Responsive
+    loop: true,
+    margin: 10,
+    nav: false,
+    dots: true,  
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      300: {
+        items: 2,
+        stagePadding: 10,
+      },
+      480: {
+        items: 2,
+        stagePadding: 18,
+      },
+      600: {
+        items: 2,
+        stagePadding: 18,
+      },
+      1000: {
+        items: 4,
+        stagePadding: 0,
+        margin: 18,
+        navText: [
+          "<i class='fa fa-chevron-left'></i>",
+          "<i class='fa fa-chevron-right'></i>",
+        ],
+        nav: true,
+        mouseDrag: true,
+        touchDrage: true,
+      },
+    },
+  });
+
 
   //Owl Carousel - blogs
   $(".related").owlCarousel({
@@ -12,60 +89,58 @@ $(document).ready(function() {
     responsive: {
       0: {
         items: 1,
-        stagePadding: 40
+        stagePadding: 10,
       },
       375: {
         items: 1,
-        stagePadding: 40
+        stagePadding: 18,
       },
       768: {
         items: 2,
-        stagePadding: 40
+        stagePadding: 18,
       },
       1024: {
-        items: 3
+        items: 3,
         // stagePadding: 0
-      }
-    }
+      },
+    },
   });
+                                                                     
 
   //Owl Carousel
-  $(".owl-carousel").owlCarousel({
+  $("#owl-two").owlCarousel({
     //Responsive
-    loop: true,
+    loop:true,
+    autoHeight:true,
     margin: 10,
     nav: false,
-    dots: true,
+    dots: false,
+    center: true,
     responsiveClass: true,
     responsive: {
       0: {
-        items: 1
+        items: 2,               
       },
-      300: {
-        items: 2,
-        stagePadding: 40
-      },
-      480: {
-        items: 2,
-        stagePadding: 40
-      },
+     
+      360: {
+        items: 2,     
+       
+      },    
       600: {
-        items: 2,
-        stagePadding: 40
+        items: 2,         
+     
       },
       1000: {
-        items: 5,
-        stagePadding: 0,
-        margin: 40,
-        navText: [
-          "<i class='fa fa-chevron-left'></i>",
-          "<i class='fa fa-chevron-right'></i>"
-        ],
-        nav: true
-      }
-    }
+        items: 3,           
+        mouseDrag: true,
+        touchDrage: true,
+      },
+    },
   });
 
+
+
+  
   // Where we located on the page
   var curPos = null;
 
@@ -81,8 +156,8 @@ $(document).ready(function() {
   // });
 
   // hide scrollbar when menu opens
-  $(".navbar-toggler").click(function() {
-    $("body").toggleClass("hide-scroll-y");
+  $(".navbar-toggler").click(function () {
+    $("body", "html").toggleClass("hide-scroll-y");
   });
 
   let winH = $("html,body").innerHeight();
@@ -90,12 +165,13 @@ $(document).ready(function() {
   let lastScroll = 0;
 
   // Menu & Search Dissapear On Scroll 1/4 Page and reappear on scrolling up
-  $(window).scroll(function() {
+  $(window).scroll(function () {
+    console.log($(this).scrollTop());
     let sTop = $(this).scrollTop();
-
+    let headerH = $("header").innerHeight();
     if ($(this).scrollTop() > winH / 10) {
       if (sTop > lastScroll) {
-        $(".page-header").css("top", `-${headerH}px`);
+        $(".page-header").css("top", `-${headerH + 15}px`);
       } else {
         $(".page-header").css("top", "0");
       }
@@ -104,10 +180,16 @@ $(document).ready(function() {
     } else {
       lastScroll = 0;
     }
+     // Show scroll-to-top button when the user scrolls to specific point
+    if($(this).scrollTop() > 300) {
+      $('.toTop').css('bottom', '30px')
+    } else {
+      $('.toTop').css('bottom', '-100px')
+    }
   });
 
   // hide navbar search by the time you scroll down to the first row of product
-  $(window).scroll(function() {
+  $(window).scroll(function () {
     if ($(".feature-products .custom-stickers") === true) {
       const firstRowH = $(".feature-products .custom-stickers").offset().top;
       const pageHeaderH = $(".page-header").innerHeight();
@@ -121,14 +203,314 @@ $(document).ready(function() {
   });
 
   // Navigate to bottom text on cliking "LEARN ABOUT CUSTOM STICKERS"
-  $(".learn-about-custome-stickers").click(function() {
+  $(".learn-about-custome-stickers").click(function () {
     const customTextOffset = $(".custom-stickers-text").offset().top;
 
     $("body, html").animate(
       {
-        scrollTop: customTextOffset
+        scrollTop: customTextOffset,
       },
       600
     );
   });
+
+  // Show Calculator Tooltip
+  $("#calculator-custom-input").bind("keydown", function () {
+    $(this).offsetParent().find(".tool-tip").removeClass("hide");
+  });
+
+  //Show width & Height Tooltip
+  $("#qq-width, #qq-height").bind("focus", function () {
+    $(this).offsetParent().find(".tool-tip").removeClass("hide");
+  });
+
+  //Hide width & Height Tooltip on clicking outside the inuput
+  $("#qq-width, #qq-height").bind("blur", function (e) {
+    hideTooltip(e);
+  });
+
+  //Hide width & Height ToolTip when user inputs right values
+  $("#qq-width").bind("keyup mouseup", function (e) {
+    let num = $(e.target).val();
+    let min = $(e.target).attr("min");
+    let max = $(e.target).attr("max");
+
+    if (num >= 0.75 && num <= 8) {
+      hideTooltip(e);
+    } else {
+      $(this).offsetParent().find(".tool-tip").removeClass("hide");
+    }
+  });
+
+  //Hide width & Height ToolTip when user inputs right values
+  $("#qq-height").bind("keyup mouseup", function (e) {
+    let num = $(e.target).val();
+    let min = $(e.target).attr("min");
+    let max = $(e.target).attr("max");
+
+    if (num >= 0.75 && num <= 11) {
+      hideTooltip(e);
+    } else {
+      $(this).offsetParent().find(".tool-tip").removeClass("hide");
+    }
+  });
+
+  // Show Calculator Tooltip Messages
+  $("#calculator-custom-input").bind("keyup mouseup", function (e) {
+    const keyCodes = [
+      8,
+      38,
+      40,
+      48,
+      49,
+      50,
+      51,
+      52,
+      53,
+      54,
+      55,
+      56,
+      57,
+      96,
+      97,
+      98,
+      99,
+      100,
+      101,
+      102,
+      103,
+      104,
+      105,
+    ];
+    const leftRightArrows = [37, 39];
+
+    let num = $("#calculator-custom-input").val();
+    let min = $("#calculator-custom-input").attr("min");
+
+    if (keyCodes.indexOf(e.which) != -1) {
+      for (let i = 10; i <= 10000; i++) {
+        if (i % 10 == 0) {
+          if (
+            (num - min >= 0 && num == i) ||
+            (num > 10 && num != i && num < 10000)
+          ) {
+            preLoader();
+
+            setTimeout(hideTooltip(e.target), 1000);
+
+            setTimeout(showPrice, 1000);
+          } else if (num == "") {
+            $(".calculator-box .tool-tip .text").text("This field is required");
+          } else if (num > i) {
+            $(".calculator-box .tool-tip .text").text(
+              "Enter value between 10 and 10000"
+            );
+          } else if (num == 0) {
+            $(".calculator-box .tool-tip .text").text(
+              "Enter value between 10 and 10000"
+            );
+          } else {
+            $(".calculator-box .tool-tip .text").text(
+              "Enter value between 10 and 10000"
+            );
+          }
+        }
+      }
+    } else if (leftRightArrows.indexOf(e.which) != -1) {
+      setTimeout(hideTooltip, 1000);
+    } else {
+      $(".calculator-box .tool-tip .text").text(
+        "Enter value between 10 and 10000"
+      );
+    }
+  });
+
+  //Hide ToolTip General Function
+  function hideTooltip(e) {
+    $(e).offsetParent("div").find(".tool-tip").addClass("hide");
+  }
+
+  // Show Price Function
+  function showPrice() {
+    let num = $("#calculator-custom-input").val();
+
+    // Calc price
+    let base = 5;
+    let multiplyer = num / 10;
+    let price = (base * multiplyer - 0.01).toFixed(2);
+
+    $(".custom-calculator-input-wrapper .discount-price .price .loader").css(
+      "display",
+      "none"
+    );
+    $(".custom-calculator-input-wrapper .discount-price .price span").text(
+      price
+    );
+  }
+
+  // Show PreLoader for a while
+  function preLoader() {
+    $(".custom-calculator-input-wrapper .discount-price .price .loader").css(
+      "display",
+      "block"
+    );
+    $(".custom-calculator-input-wrapper .discount-price .price span").text("");
+  }
+
+  // Add custome css when carousel viewing less than 5 products
+  const products = document.querySelectorAll(".owl-carousel .card");
+  const owlStage = document.querySelectorAll(".owl-stage");
+
+  //OnLoad
+  window.addEventListener("load", () => {
+    owlStage.forEach((owl) => {
+      if (products.length < 5) {
+        owl.lastChild.style.marginRight = "0";
+      }
+
+      if (products.length > 2) {
+        if (window.innerWidth < 992) {
+          owl.style.marginLeft = "-40px";
+        }
+      }
+    });
+  });
+
+  //OnResize
+  window.addEventListener("resize", () => {
+    owlStage.forEach((owl) => {
+      if (products.length < 5) {
+        owl.lastChild.style.marginRight = "0";
+      }
+
+      if (products.length > 2) {
+        if (window.innerWidth < 992) {
+          owl.style.marginLeft = "-40px";
+        } else {
+          owl.style.marginLeft = "0";
+        }
+      }
+    });
+
+  });
+
+  // show secondary menu on hovering dropdown links
+  const menuItem = document.querySelectorAll(
+    ".dropdown-menu .menu_list .dropdown-item"
+  );
+
+  // menuItem.forEach(e => {
+
+  // e.addEventListener('mouseover', e => {
+
+  // const secondaryMenu = `${e.currentTarget.getAttribute('data-show')}`
+
+  // $(`#${secondaryMenu}`).addClass('animate');
+
+  // });
+
+  // e.addEventListener('mouseleave', e => {
+
+  //   const secondaryMenu = `${e.currentTarget.getAttribute('data-show')}`
+
+  //   $(`#${secondaryMenu}`).removeClass('animate');
+
+  // });
+
+  // })
+
+  //show main menu dropdown on hovering navbar links
+  const navLinks = document.querySelectorAll(".collapse .dropdown");
+
+  navLinks.forEach((e) => {
+    // Show Menu Dropdwon
+    e.addEventListener("mouseover", (e) => {
+      const menuDropDown = e.currentTarget.querySelector(".dropdown-menu");
+
+      $(e.currentTarget).addClass("show");
+
+      $(menuDropDown).addClass("show");
+    });
+
+    //Hide Menu Dropdown
+    e.addEventListener("mouseleave", (e) => {
+      const menuDropDown = e.currentTarget.querySelector(".dropdown-menu");
+
+      $(e.currentTarget).removeClass("show");
+
+      $(menuDropDown).removeClass("show");
+    });
+  });
+
+  // Mobile menu slide from the right
+  const mobileMenuItem = document.querySelectorAll(
+    ".mobile-menu .menu-links .menu-item > a"
+  );
+  const backwardItem = document.querySelectorAll(".backward > a");
+  const mobileSubMenuItem = document.querySelectorAll(
+    "#mobileShopMenu .menu_list a"
+  );
+
+  //slide in form the right
+  mobileMenuItem.forEach((e) => {
+    e.addEventListener("click", (e) => {
+      document.querySelector(`${e.currentTarget.hash}`).classList.add("slided");
+      e.preventDefault();
+    });
+  });
+
+  //slide out form the left
+  backwardItem.forEach((e) => {
+    e.addEventListener("click", (e) => {
+      e.currentTarget.offsetParent.classList.remove("slided");
+    });
+  });
+
+  //Slide subMenu From the right
+  mobileSubMenuItem.forEach((e) => {
+    e.addEventListener("click", (e) => {
+      document.querySelector(`${e.currentTarget.hash}`).classList.add("slided");
+      e.preventDefault();
+    });
+  });
+
+  //Select between options
+  $('.popup-options .option').each(function () {
+    $(this).click(function(e) {
+      $(e.currentTarget).find('input').attr('aria-checked', 'true');
+      $(e.currentTarget).parent().siblings().find('input').attr('aria-checked', 'false');
+      $('.popup-options input[aria-checked="false"]').removeAttr('checked').parent().parent().find('.option_img').removeClass('selected')
+      $('.popup-options input[aria-checked="true"]').parent().parent().find('.option_img').addClass('selected')
+    })
+  });
+
+  //close pop-up window
+  $('.popup-header .close-icon , .popup-content .cancel').click(function() {
+    $('.popup-window, .overlay').fadeOut()
+  });
+
+  //open popup window
+  $('.stickers-wrapper .sticker a, .order-table .product_img').each(function() {
+    $(this).click(function() {
+      $('.popup-window, .overlay').fadeIn()
+    })
+  });
+
+  //Expand  the contracted BreadCrumb items
+  $('.breadcrumb').each(function() {
+    $(this).click(function() {
+      $('.contract').toggleClass('contracted');
+      $(this).find('.target').toggleClass('contracted');
+    })
+  });
+
+  //Tooltip
+  document.querySelectorAll('[data-toggle="tooltip"]').forEach(function(e) {
+    let target = document.querySelector(`.${e.getAttribute('data-toggle')}`);
+    //mouse hover
+    e.addEventListener('mouseenter', function() {target.classList.add('show')});
+    e.addEventListener('mouseleave', function() {target.classList.remove('show')});
+  })
+
+ 
 });
--- js/scripts.js
+++ js/scripts.js
@@ -36,6 +36,15 @@
 (function ($) {
   "use strict";
 
+  if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+    $('.primary-nav.desktop, .primary-nav.mobile').removeClass('d-none').addClass('d-flex');
+    $('.primary-nav.desktop').css('display', 'flex !important');
+    $('.primary-nav, .secondary-nav .currency.dropdown, .nav-group .search').hide();
+    $('.secondary-nav a button').addClass('d-none');
+    $('.page-header .container > .row').removeClass('flex-column');
+    $('.primary-nav').removeClass('w-100')
+  }
+
   // mobile menu login button
   var mobileLoginButton = $("#mobile-menu-login-button");
   // mobile my accout button text
@@ -66,6 +75,27 @@
   //   $(".quote-calculator .calculator-bg #calculator-custom-input").val()
   // );
 
+  // onload
+  $('a[data-toggle=collapse]').each(function () {
+    var chevron = $(this).parent().next('div');
+    if (chevron.hasClass('in')) {
+      $(this).children('i').removeClass('fa-chevron-down').addClass('fa-chevron-up');
+    } else {
+      const id = this.id
+      if (id === 'a-support') {
+        $(this).children('i').removeClass('fa-chevron-down').addClass('fa-chevron-up');
+        return
+      }
+      $(this).children('i').removeClass('fa-chevron-up').addClass('fa-chevron-down');
+    }
+  });
+
+  // click event
+  $('a[data-toggle=collapse]').click(function () {
+    // swap chevron
+    $(this).children('i').toggleClass('fa-chevron-down fa-chevron-up');
+  });
+
   // verify if inner text is Login or Logout and adjust on click
   mobileLoginButton.click(function () {
     $(this).text($(this).text() === "Login" ? "Logout" : "Login");
@@ -85,6 +115,7 @@
     $("#calculator-custom-input").focus();
     selectedInput.removeClass("activated-calc-input");
     $("i.calc-check").addClass("hide");
+    $(".custom-calculator-input-wrapper .discount-price .price i.calc-check").removeClass('hide');
   });
 
   // Highlight - select from calculator
@@ -125,13 +156,36 @@
       $(".custom-calculator-input-wrapper").hide();
       customQty.show();
     }
+
+    $('.tool-tip').addClass('hide');
   });
 
   // Hide menu when resizeing the screen to desktop view
+  const guest = $('.category-items.guest');
+  const host  = $('.dropdown-menu.host');
+  const sibling  = $('h4.sibling');
+  // Move the category list into a dropdown on mobile screen
+  if ($(window).innerWidth() <= 768) {
+    $('.category-items.guest').appendTo(host);
+    $('.art-asset .row.guest').insertBefore('.art-asset .row.host')
+  } else {
+    $('.category-items.guest').insertAfter(sibling);
+    $('.art-asset .row.guest').prependTo('.section-content')
+  }
+
   $(window).resize(function () {
     if ($(this).innerWidth() > 991) {
       $(".mobile-menu").removeClass("show");
       $(".primary-nav.desktop .navbar-toggler").attr("aria-expanded", "false");
+    }
+
+    // Move the category list into a dropdown on mobile screen
+    if ($(this).innerWidth() <= 768) {
+      $('.category-items.guest').appendTo(host);
+      $('.art-asset .row.guest').insertBefore('.art-asset .row.host')
+    } else {
+      $('.category-items.guest').insertAfter(sibling);
+      $('.art-asset .row.guest').prependTo('.section-content')
     }
   });
 
@@ -155,4 +209,124 @@
       window.location.href = "#";
     }
   });
+
+  // Order Option Image Change & selection
+
+  var onHoverSame = function () {
+    if ($('input:radio[name="order-option"]').is(":checked")) {
+      return;
+    }
+    $("img#same-design-img").attr(
+      "src",
+      "/img/stickermaker/HowWillYouOrder_Wizard_Selected.png"
+    );
+  };
+
+  var offHoverSame = function () {
+    if ($('input:radio[name="order-option"]').is(":checked")) {
+      return;
+    }
+    $("img#same-design-img").attr(
+      "src",
+      "/img/stickermaker/HowWillYouOrder_Wizard.png"
+    );
+  };
+
+  var onHoverMultiple = function () {
+    if ($('input:radio[name="order-option"]').is(":checked")) {
+      return;
+    }
+    $("img#many-design-img").attr(
+      "src",
+      "/img/stickermaker/HowWillYouOrder_PageMaker_Selected.png"
+    );
+  };
+
+  var offHoverMultiple = function () {
+    if ($('input:radio[name="order-option"]').is(":checked")) {
+      return;
+    }
+    $("img#many-design-img").attr(
+      "src",
+      "/img/stickermaker/HowWillYouOrder_PageMaker.png"
+    );
+  };
+
+  // Hover Effect For Same Design
+  $("img#same-design-img").hover(onHoverSame, offHoverSame);
+  // Hover Effect Multiple
+  $("img#many-design-img").hover(onHoverMultiple, offHoverMultiple);
+
+  // Selected Change
+
+  $('input:radio[name="order-option"]').change(function () {
+    if ($(this).is(":checked") && $(this).val() == "same-design") {
+      //  Grey out multiple image
+      $("img#many-design-img").attr(
+        "src",
+        "/img/stickermaker/HowWillYouOrder_PageMaker.png"
+      );
+      // Color Single Single Image Option
+      $("img#same-design-img").attr(
+        "src",
+        "/img/stickermaker/HowWillYouOrder_Wizard_Selected.png"
+      );
+    } else {
+      //  grey ou single image
+      $("img#same-design-img").attr(
+        "src",
+        "/img/stickermaker/HowWillYouOrder_Wizard.png"
+      );
+      // Color Multiple Option
+      $("img#many-design-img").attr(
+        "src",
+        "/img/stickermaker/HowWillYouOrder_PageMaker_Selected.png"
+      );
+    }
+  });
+
+
+  let item_length = $(".custom-stickers .content-item").length;
+  if (item_length < 6) {
+    $(".view-item").hide();
+    $(".content-item").show();
+  } else {
+    $('.custom-stickers .content-item:lt(5)').show();
+    $(".view-item").show();
+
+  }
+
+  $(".view-item").on("click", function (e) {
+    e.preventDefault();
+    $(".content-item:hidden").slideDown();
+    $(".view-item").hide();
+    $(".lessCount").show();
+
+  });
+
+  $(".lessCount").on("click", function (e) {
+    e.preventDefault();
+    $(".content-item").hide();
+    $('.custom-stickers .content-item:lt(5)').show();
+    $(".lessCount").hide();
+    $(".view-item").show();
+  });
+
+  // toggle between selected category when user clicks
+  $('.category-items .item').each(function() {
+    const dropdownHeader = $('.selected-category .dropdown .btn .dropdown-title');
+    $(this).on('click', function(e) {
+      e.preventDefault()
+      $(e.currentTarget).addClass('selected').parent().siblings().find('.item').removeClass('selected');
+      // Change the selected category on dropdown header 
+      let imgSrc = $(e.currentTarget).find('img').attr('src')
+      let title = $(e.currentTarget).find('span').html();
+      dropdownHeader.find('img').attr('src', `${imgSrc}`);
+      dropdownHeader.find('h4').html(`${title}`);
+     
+    })
+  })
+
+
+
 })(jQuery);
